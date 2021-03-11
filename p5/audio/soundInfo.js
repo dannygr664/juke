4e861bfbd7fbd455cb68dd7574f08c617276c990
Danny Greenberg <greenberg08@gmail.com>
2021-03-11 20:44:50 +0100

    Add SoundInfo and AudioFilePathParser classes

---
+++ p5/audio/soundInfo.js
@@ -0,0 +1,10 @@
+class SoundInfo {
+  constructor(genre, partName, bpm, timeSignatureUpper, timeSignatureLower, length) {
+    this.genre = genre;
+    this.partName = partName;
+    this.bpm = bpm;
+    this.timeSignatureUpper = timeSignatureUpper;
+    this.timeSignatureLower = timeSignatureLower;
+    this.length = length;
+  }
+}
