d9460ee0aef6c94e19d6e4f0aaf699b591fefd81
Danny Greenberg <greenberg08@gmail.com>
2021-03-30 19:45:03 +0200

    Adjust fluid animations based on speed

--- p5/animations/boxes.js
+++ p5/animations/boxes.js
@@ -14,6 +14,24 @@ class Boxes {
   draw(rms) {
     fill(player.isReviving ? ColorScheme.BLACK_INACTIVE : this.color, agentAlpha);
 
+    let colorScaleFactor;
+    switch (this.color) {
+      case ColorScheme.LOFI_HIGHEST_SPEED:
+        colorScaleFactor = 2;
+        break;
+      case ColorScheme.LOFI_HIGHER_SPEED:
+        colorScaleFactor = 1.5;
+        break;
+      case ColorScheme.LOFI_LOWER_SPEED:
+        colorScaleFactor = 1;
+        break;
+      case ColorScheme.LOFI_LOWEST_SPEED:
+        colorScaleFactor = 0.7;
+        break;
+      default:
+        colorScaleFactor = 1;
+    }
+
     for (let gridY = 0; gridY < (this.y2 - this.y1); gridY += 25) {
       for (let gridX = 0; gridX < (this.x2 - this.x1); gridX += 25) {
         let diameter = dist(
@@ -22,7 +40,7 @@ class Boxes {
           gridX,
           gridY
         );
-        diameter = diameter / maxDistance * 50;
+        diameter = (diameter / maxDistance) * 50 * colorScaleFactor;
         push();
         translate(this.x1 + gridX, gridY, diameter);
         rect(0, 0, diameter, diameter); // also nice: ellipse(...)
