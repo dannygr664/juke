954a219d035775a13ee973540d072b0b30a14215
Danny Greenberg <greenberg08@gmail.com>
2021-04-19 17:23:05 +0200

    Remove unused code

--- p5/animations/moire.js
+++ p5/animations/moire.js
@@ -11,15 +11,9 @@ class Moire {
 
   drawMoire() {
     let lineHeight = dist(this.x1, this.y1, this.x2, this.y2);
-    //let lineAngle = atan2(this.y2 - this.y1, this.x2 - this.x1);
     push();
     stroke(this.color);
     strokeWeight(5);
-
-
-    //translate(this.x1, this.y1);
-    //rotate(lineAngle);
-    //translate(0, -this.width / 2);
 
     for (var i = 0; i < this.width; i += this.width / 5) {
       line(this.x1 + i, 0, this.x1 + i, lineHeight);
